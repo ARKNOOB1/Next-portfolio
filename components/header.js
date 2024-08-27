524c24cb1f1bfb3741f022956a8de66faf0bc194
ARKNOOB1 <a59114154@gmail.com>
2024-08-27 20:11:38 +0900

    8_27

--- components/header.js
+++ components/header.js
@@ -1,7 +1,23 @@
+import Link from 'next/link'
+import styles from './header.module.css'
+import Image from 'next/image'
+
 export default function Header() {
     return(
-        <div className="header">
-            <h1>헤더입니다.</h1>
+        <div className={styles.header}>
+            <nav className={styles.navbar}>
+                <Link href='/' className={styles.nav_logo}>
+                    <Image src='/Logo.jpg' width={64} height={64}></Image>
+                    <span>Portfolio</span>
+                </Link>
+                <div className={styles.nav_menu}>
+                    <ul className={styles.nav_list}>
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/about-me'>Projects</Link></li>
+                        <li><Link href='/404'>Content</Link></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,9 @@
+import Layout from '../components/layout'
+
 export default function PageNotFound() {
     return(
-        <>
+        <Layout>
             <h1>404 Error</h1>
-        </>
+        </Layout>
     )
 }
