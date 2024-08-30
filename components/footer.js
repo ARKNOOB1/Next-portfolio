6f960473f982d22664c183413ec84265716cd10d
ARKNOOB1 <a59114154@gmail.com>
2024-08-30 14:13:57 +0900

    8_30-3.5

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,8 @@
+import styles from './footer.module.css';
+
 export default function Footer() {
     return(
-        <div className="footer">
+        <div className={styles.footer}>
             <h1>푸터입니다.</h1>
         </div>
     )
